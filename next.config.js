be074cc902a9eff97968e8bceddbd349bbc62d33
しょうた <99630778+shouta0715@users.noreply.github.com>
2024-03-19 16:39:25 +0900

    setting deploy github pages with next.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/dclab' : '',
 };
 
 module.exports = withBundleAnalyzer(nextConfig)
