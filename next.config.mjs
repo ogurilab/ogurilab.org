bd1afbd544a9c21a262636f03832242a1788d083
shouta0715 <Myheroes0715@gmail.com>
2024-03-20 13:21:17 +0900

    add: mdx loader

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+import remarkGfm from "remark-gfm";
+import createMDX from "@next/mdx";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  images: {
+    loader: "custom",
+    loaderFile: "./image-loader.ts",
+  },
+};
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+  },
+});
+
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})(withMDX(nextConfig));
