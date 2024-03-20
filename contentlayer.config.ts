3a904afd5dd76df0dd1587cc3e4760eaabd3207f
shouta0715 <Myheroes0715@gmail.com>
2024-03-21 00:32:30 +0900

    add: mdx contentlayer

---
+++ contentlayer.config.ts
@@ -0,0 +1,44 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import remarkGfm from "remark-gfm";
+
+export const news = defineDocumentType(() => ({
+  name: "News",
+  filePathPattern: "**/*.mdx",
+  contentType: "mdx",
+  mdx: {
+    rehypePlugins: [remarkGfm],
+  },
+  fields: {
+    title: {
+      type: "string",
+      description: "タイトル",
+      required: true,
+    },
+    href: {
+      type: "string",
+      description: "URL",
+      required: true,
+    },
+    category: {
+      type: "string",
+      description: "カテゴリー",
+      required: true,
+    },
+    date: {
+      type: "string",
+      description: "日付",
+      required: true,
+    },
+    image: {
+      type: "string",
+      description:
+        "画像 public/news に画像を配置し、その画像名(example.jpg)を指定",
+      required: true,
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: "src/docs/news",
+  documentTypes: [news],
+});
