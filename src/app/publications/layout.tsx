882126287cca142ee8f6c1e4a3b6a3ddb7416406
shouta0715 <Myheroes0715@gmail.com>
2024-03-21 15:32:50 +0900

    fix publications fadein

--- src/app/publications/layout.tsx
+++ src/app/publications/layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FadeInWithStagger } from "@/components/fade-in";
 import { MDXDescription, MDXHeader, MDXTitle } from "@/components/mdx";
 import { Pattern } from "@/components/patterns";
 
@@ -12,7 +13,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           本ページでは、小栗研究室の学術活動の成果を紹介しています。小栗先生をはじめとする研究室メンバーによる最新の研究論文、技術記事、国際会議での発表、受賞歴など、私たちの研究の進展と貢献を広く共有する場としています。
         </MDXDescription>
       </MDXHeader>
-      <div className="leading-7">{children}</div>
+      <FadeInWithStagger className="leading-7">{children}</FadeInWithStagger>
     </div>
   );
 }
