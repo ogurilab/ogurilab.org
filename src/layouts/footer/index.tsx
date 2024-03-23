8d1ed1e7c0078c86088f53f10b1ce324a50ca641
shouta0715 <Myheroes0715@gmail.com>
2024-03-23 21:02:30 +0900

    add: footer outer links

--- src/layouts/footer/index.tsx
+++ src/layouts/footer/index.tsx
@@ -30,12 +30,39 @@ const social = [
   },
 ];
 
+const outreLinks = [
+  {
+    name: "愛知工業大学",
+    href: "https://www.ait.ac.jp/",
+  },
+  {
+    name: "愛知工業大学 情報科学部",
+    href: "https://www.ait.ac.jp/faculty/info-science/index.html",
+  },
+  {
+    name: " 小栗真弥 個人サイト",
+    href: "https://shinyaoguri.com/",
+  },
+  {
+    name: "雑居ゼミ",
+    href: "https://zakkyo.com/",
+  },
+  {
+    name: "全国登文会",
+    href: "https://zen-toubunkai.com/",
+  },
+  {
+    name: "愛知登文会",
+    href: "http://www.aichi-tobunkai.org/index.html",
+  },
+];
+
 export function Footer() {
   return (
     <footer className="mt-32 border-t border-border">
       <div className="relative mx-auto max-w-7xl px-4 py-24 sm:px-6 lg:px-8">
         <nav aria-label="Footer">
-          <FadeInWithStagger className="-mb-6 grid grid-cols-2 grid-rows-4 items-center justify-between md:flex md:grid-cols-1 md:grid-rows-1 md:justify-center">
+          <FadeInWithStagger className="grid grid-cols-2 grid-rows-4 items-center justify-between md:flex md:grid-cols-1 md:grid-rows-1 md:justify-center">
             {navigation.map((item) => (
               <NavItem
                 key={item.label}
@@ -48,6 +75,25 @@ export function Footer() {
             ))}
           </FadeInWithStagger>
         </nav>
+        <div className="mt-10">
+          <p className="px-2 text-sm font-bold tracking-wider md:text-center lg:px-3">
+            外部リンク
+          </p>
+          <ul className="grid grid-cols-2 grid-rows-4 items-center justify-between md:flex md:grid-cols-1 md:grid-rows-1 md:justify-center">
+            {outreLinks.map((item) => (
+              <li key={item.name} className="relative block p-3 px-2 lg:px-3">
+                <a
+                  className="text-sm text-muted-foreground underline-offset-4 hover:underline"
+                  href={item.href}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
         <div className="mt-10 flex justify-center space-x-10">
           {social.map((item) => (
             <a
