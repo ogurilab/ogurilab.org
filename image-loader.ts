302222ccd73fb3db480768653e4364cb059416f1
shouta0715 <Myheroes0715@gmail.com>
2024-03-27 17:13:55 +0900

    revert image loader

--- image-loader.ts
+++ image-loader.ts
@@ -1,20 +1,19 @@
 import { ImageProps } from "next/image";
 
-// const baseUrl = process.env.BASE_URL ?? process.env.NEXT_PUBLIC_BASE_URL;
+const baseUrl = process.env.BASE_URL ?? process.env.NEXT_PUBLIC_BASE_URL;
 
-// const resizeEndpoint = process.env.ENDPOINT ?? process.env.NEXT_PUBLIC_ENDPOINT;
+const resizeEndpoint = process.env.ENDPOINT ?? process.env.NEXT_PUBLIC_ENDPOINT;
 
-export default function imageLoader({ src }: ImageProps) {
-  return src;
-  // const isRelative = src.toString().startsWith("/");
+export default function imageLoader({ src, width, quality }: ImageProps) {
+  const isRelative = src.toString().startsWith("/");
 
-  // const query = new URLSearchParams();
+  const query = new URLSearchParams();
 
-  // if (width) query.set("width", width.toString());
+  if (width) query.set("width", width.toString());
 
-  // query.set("quality", (quality || 70).toString());
+  query.set("quality", (quality || 70).toString());
 
-  // const url = isRelative ? `${baseUrl}${src.toString()}` : src.toString();
+  const url = isRelative ? `${baseUrl}${src.toString()}` : src.toString();
 
-  // return `${resizeEndpoint}?url=${url}&${query.toString()}`;
+  return `${resizeEndpoint}?url=${url}&${query.toString()}`;
 }
