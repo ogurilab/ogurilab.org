8a96d7dc0d3f389dc708fd5646e5bccb837f5351
しょうた <99630778+shouta0715@users.noreply.github.com>
2024-04-18 19:56:54 +0900

    newsの取得する記事を変更 (#34)
    * fix: fetch new articles
    
    * fix: fetch news type

--- src/app/about/_components/news/index.tsx
+++ src/app/about/_components/news/index.tsx
@@ -7,7 +7,7 @@ import { fetchNews } from "@/lib/fetch";
 import { getGyazoImage, timestampToDate } from "@/lib/utils";
 
 export async function OurNews() {
-  const { pages } = await fetchNews(3);
+  const pages = await fetchNews();
 
   return (
     <div className="mx-auto max-w-7xl">
@@ -20,7 +20,7 @@ export async function OurNews() {
         </p>
       </div>
       <FadeInWithStagger className="mx-auto mt-6 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-        {pages.map((news) => (
+        {pages.slice(0, 3).map((news) => (
           <FadeIn key={news.id}>
             <article className="relative isolate flex h-full flex-col justify-end overflow-hidden rounded-2xl bg-primary px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
               <Image
