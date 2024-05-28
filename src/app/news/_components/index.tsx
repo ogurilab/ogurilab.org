c9645364c30a40db0ee3fe216f7d060251d95dda
shouta0715 <Myheroes0715@gmail.com>
2024-05-28 20:09:48 +0900

    feat: tags

--- src/app/news/_components/index.tsx
+++ src/app/news/_components/index.tsx
@@ -6,7 +6,7 @@ import { useSearchParams } from "next/navigation";
 import React from "react";
 import { FadeIn, FadeInWithStagger } from "@/components/fade-in";
 import { Image } from "@/components/ui/image";
-import { fetchNews } from "@/lib/fetch";
+import { fetchNews, tags } from "@/lib/fetch";
 import { getGyazoImage, timestampToDate } from "@/lib/utils";
 
 type NewsProps = {
@@ -49,56 +49,73 @@ export function News({ data }: NewsProps) {
         className="grid grid-cols-2 gap-x-8 gap-y-20 lg:grid-cols-3"
         speed={0.05}
       >
-        {currentPages.map((news, i) => (
-          <FadeIn key={news.title} className="overflow-hidden">
-            <article className="flex flex-col items-start justify-between">
-              <figure className="h-52 w-full overflow-hidden rounded-2xl border border-border">
-                <a
-                  className="group"
-                  href={`https://scrapbox.io/dclab/${news.title}`}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <Image
-                    alt={`${news.title}`}
-                    className="size-full object-cover object-center transition-opacity group-hover:opacity-75"
-                    height={200}
-                    priority={i < 3}
-                    src={getGyazoImage(news.image)}
-                    width={300}
-                  />
-                  <span className="sr-only">{news.title}</span>
-                </a>
-              </figure>
-              <div className="flex-1">
-                <div className="mt-4 flex flex-col gap-1 gap-x-4 text-xs sm:flex-row sm:items-center">
-                  <time
-                    className="mr-8"
-                    dateTime={timestampToDate(news.created).toISOString()}
+        {currentPages.map((news, i) => {
+          const probableTag = tags.find((tag) => news.linksLc.includes(tag));
+
+          const tag = probableTag ?? news.linksLc.at(-1) ?? "その他";
+
+          return (
+            <FadeIn key={news.title} className="overflow-hidden">
+              <article className="flex flex-col items-start justify-between">
+                <figure className="h-52 w-full overflow-hidden rounded-2xl border border-border">
+                  <a
+                    className="group"
+                    href={`https://scrapbox.io/dclab/${news.title}`}
+                    rel="noopener noreferrer"
+                    target="_blank"
                   >
-                    {timestampToDate(news.created).toLocaleDateString("ja-JP", {
-                      year: "numeric",
-                      month: "long",
-                      day: "numeric",
-                    })}
-                  </time>
-                </div>
-                <div className="group relative">
-                  <h2 className="mt-3  text-base font-semibold leading-7 tracking-tight">
+                    <Image
+                      alt={`${news.title}`}
+                      className="size-full object-cover object-center transition-opacity group-hover:opacity-75"
+                      height={200}
+                      priority={i < 3}
+                      src={getGyazoImage(news.image)}
+                      width={300}
+                    />
+                    <span className="sr-only">{news.title}</span>
+                  </a>
+                </figure>
+                <div className="flex-1">
+                  <div className="mt-4 flex gap-1 text-xs">
+                    <time
+                      dateTime={timestampToDate(news.created).toISOString()}
+                    >
+                      {timestampToDate(news.created).toLocaleDateString(
+                        "ja-JP",
+                        {
+                          year: "numeric",
+                          month: "long",
+                          day: "numeric",
+                        }
+                      )}
+                    </time>
+                    /
                     <a
-                      className="line-clamp-3 underline-offset-4 hover:underline "
-                      href={`https://scrapbox.io/dclab/${news.title}`}
+                      className="underline-offset-4 hover:underline"
+                      href={`https://scrapbox.io/dclab/${tag}`}
                       rel="noopener noreferrer"
                       target="_blank"
                     >
-                      {news.title}
+                      {tag}
                     </a>
-                  </h2>
+                  </div>
+                  <div className="group relative">
+                    <h2 className="mt-3  text-base font-semibold leading-7 tracking-tight">
+                      <a
+                        className="line-clamp-3 underline-offset-4 hover:underline "
+                        href={`https://scrapbox.io/dclab/${news.title}`}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        {news.title}
+                      </a>
+                    </h2>
+                  </div>
                 </div>
-              </div>
-            </article>
-          </FadeIn>
-        ))}
+              </article>
+            </FadeIn>
+          );
+        })}
       </FadeInWithStagger>
 
       <div className="mt-8 flex justify-center space-x-8">
