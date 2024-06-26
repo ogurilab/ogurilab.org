274e8cf72d8e468d544b39d6dd79921174866e1c
shouta0715 <Myheroes0715@gmail.com>
2024-06-26 12:18:31 +0900

    fix: sort Algorithm

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -19,7 +19,31 @@ type Res = {
   };
 };
 
-const sortCreatedAt = (a: Pages, b: Pages) => b.created - a.created;
+const regex = /^\d{4}-\d{2}-\d{2}$/;
+
+export const getDateString = (linksLc: string[]) => {
+  let date;
+
+  for (const link of linksLc) {
+    if (regex.test(link)) {
+      date = link;
+      break;
+    }
+  }
+
+  return date;
+};
+
+const sortCreatedAt = (a: Pages, b: Pages) => {
+  const dateA = getDateString(a.linksLc);
+  const dateB = getDateString(b.linksLc);
+
+  if (dateA && dateB) {
+    return dateB.localeCompare(dateA);
+  }
+
+  return b.created - a.created;
+};
 
 export const tags = [
   "ニュース",
