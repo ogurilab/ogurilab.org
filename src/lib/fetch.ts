64ca70a5e22d8b71039f90414f7c568cbf1fa9cb
しょうた <99630778+shouta0715@users.noreply.github.com>
2024-06-26 18:20:12 +0900

    Newsの並び順を変更 (#52)
    * fix: sort Algorithm
    
    * fix: 日付の表示を変更
    
    * chore: fix error
    
    * fix: タイトルから取得するように変更

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -19,7 +19,24 @@ type Res = {
   };
 };
 
-const sortCreatedAt = (a: Pages, b: Pages) => b.created - a.created;
+const regex = /\d{4}-\d{2}-\d{2}/;
+
+export const getDateString = (title: string) => {
+  const match = title.match(regex);
+
+  return match ? match[0] : null;
+};
+
+const sortCreatedAt = (a: Pages, b: Pages) => {
+  const dateA = getDateString(a.title);
+  const dateB = getDateString(b.title);
+
+  if (dateA && dateB) {
+    return dateB.localeCompare(dateA);
+  }
+
+  return b.created - a.created;
+};
 
 export const tags = [
   "ニュース",
@@ -31,11 +48,22 @@ export const tags = [
   "対外発表・受賞",
 ];
 
-export const fetchNews = async () => {
+export const fetchNews = async (): Promise<(Pages & { date: Date })[]> => {
   const res = await fetch("https://scrapbox.io/api/pages/dclab/news", {
     cache: "force-cache",
   });
   const data = (await res.json()) as Res;
 
-  return data.relatedPages.links1hop.sort(sortCreatedAt);
+  const sorted = data.relatedPages.links1hop.sort(sortCreatedAt);
+
+  const news = sorted.map((page) => {
+    const date = getDateString(page.title);
+
+    return {
+      ...page,
+      date: date ? new Date(date) : new Date(page.created * 1000),
+    };
+  });
+
+  return news;
 };
